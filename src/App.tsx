2aeab2fdfaf9acc9c3493f8d2d60ec112a6888f3
Trifall <JerrenT@Trifall.com>
2024-03-04 05:51:36 -0500

    update packages and add new parallax package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import { Parallax } from 'react-scroll-parallax';
+import Parallax from 'react-next-parallax';
 import Transition from './components/LandingEntry';
 import SmoothLink from './components/SmoothLink';
 
@@ -13,12 +13,10 @@ function App() {
 						<div className='relative m-6 flex w-full items-center justify-center overflow-hidden rounded-lg bg-black bg-opacity-75'>
 							<canvas className='orb_canvas'></canvas>
 							<div className='absolute left-1/2 top-1/2 z-10 w-[85%] -translate-x-1/2 -translate-y-1/2 p-4 sm:w-[80%] sm:p-32'>
-								<Parallax speed={-5} style={{ position: 'relative' }}>
-									<motion.span className='text-2xl leading-snug text-white sm:text-4xl sm:text-[2.3vw]'>
-										Howdy Partner! I&apos;m Jerren Trifan, a web and software developer. I love creating software
-										solutions in many different languages and frameworks.
-									</motion.span>
-								</Parallax>
+								<motion.span className='text-2xl leading-snug text-white sm:text-4xl sm:text-[2.3vw]'>
+									Howdy Partner! I&apos;m Jerren Trifan, a web and software developer. I love creating software
+									solutions in many different languages and frameworks.
+								</motion.span>
 							</div>
 							{/* My Projects Learn more Button */}
 							<div className='absolute bottom-16 right-16 z-10'>
@@ -34,14 +32,17 @@ function App() {
 						</div>
 					</div>
 					<div className='relative flex h-screen w-full items-center justify-center bg-black' id='projects'>
-						<Parallax speed={-5} translateY={[300, -300]} className='bg-red-500' style={{ position: 'relative' }}>
-							<motion.span className='text-4xl leading-snug text-white sm:text-[2.3vw]'>
-								add project card thing here!
-							</motion.span>
+						<Parallax style={{ position: 'relative' }}>
+							<div className='relative h-[600px] w-[800px] [&>img]:absolute [&>img]:inset-0 [&>span]:absolute [&>span]:inset-0'>
+								<img src='https://via.placeholder.com/800x600' alt='placeholder' data-parallax-offset='-2' />
+								<span className='px-3 text-4xl leading-snug text-white sm:text-[2.3vw]' data-parallax-offset='2'>
+									add project card thing here!
+								</span>
+							</div>
 						</Parallax>
 					</div>
 					<div className='relative h-screen w-full items-center justify-center bg-black' id='projects'>
-						<Parallax speed={-5} translateY={[300, -300]} style={{ position: 'relative' }}>
+						<Parallax style={{ position: 'relative' }}>
 							<motion.span className='text-4xl leading-snug text-white sm:text-[2.3vw]'>
 								add project card thing here!
 							</motion.span>
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,13 @@ import '@/styles/global.css';
 import { ThemeProvider } from '@/styles/theme-provider.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ParallaxProvider } from 'react-scroll-parallax';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<ParallaxProvider>
-			<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-				<App />
-			</ThemeProvider>
-		</ParallaxProvider>
+		<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
 
